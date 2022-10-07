ccdd28258c99c518fe33066dae454c59bf8f1d80
Nicolas BAUW <nbauw@hotmail.com>
2022-10-07 15:13:39 +0200

    Printing the whole screen

--- src/display.rs
+++ src/display.rs
@@ -2,6 +2,7 @@ use sdl2::rect::Rect;
 use sdl2::pixels::Color;
 use sdl2::render::Canvas;
 use sdl2_unifont::renderer::SurfaceRenderer;
+use std::borrow::Cow;
 use std::str;
 
 pub fn display(canvas: &mut Canvas<sdl2::video::Window>, bytes: Vec<u8>) {
@@ -22,7 +23,7 @@ pub fn display(canvas: &mut Canvas<sdl2::video::Window>, bytes: Vec<u8>) {
         s.push(String::from_utf8_lossy(&line));
     }
     
-    let surf = draw_line(&s[0]);
+    let surf = draw_lines(s);
 
     let texture_creator = canvas.texture_creator();
     let text_tex = texture_creator
@@ -32,19 +33,22 @@ pub fn display(canvas: &mut Canvas<sdl2::video::Window>, bytes: Vec<u8>) {
     
 }
 
-fn draw_line(chars: &str) -> sdl2::surface::Surface {
+fn draw_lines(chars: Vec<Cow<str>>) -> sdl2::surface::Surface {
+    let mut start:i32 = 2;
     let mut screen = sdl2::surface::Surface::new(
         800,
         600,
         sdl2::pixels::PixelFormatEnum::RGBA8888,
     ).unwrap();
 
-    let renderer = SurfaceRenderer::new(Color::RGB(255, 255, 255), Color::RGB(0, 0, 0));
-    renderer
-        .draw(chars)
-        .unwrap()
-        .blit(None, &mut screen, Rect::new(2, 2, 0, 0))
-        .unwrap();
-
-        screen
+    for s in chars.iter() {
+        let renderer = SurfaceRenderer::new(Color::RGB(255, 255, 255), Color::RGB(0, 0, 0));
+        renderer
+            .draw(&s)
+            .unwrap()
+            .blit(None, &mut screen, Rect::new(2, start, 0, 0))
+            .unwrap();
+        start += 20;
+    }
+    screen
 }
