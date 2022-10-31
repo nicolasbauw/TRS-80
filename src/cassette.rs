8e031d39a40df32a76b3ef8086d0851c1ea29858
Nicolas BAUW <nbauw@hotmail.com>
2022-10-31 14:42:45 +0100

    Sync pulse + MSB first

--- src/cassette.rs
+++ src/cassette.rs
@@ -1,8 +1,9 @@
 pub fn serialize(input: &[u8]) -> Vec<u8> {
     let mut bits = Vec::new();
     for byte in input.iter() {
-        for bit in 0..=7 {
-            bits.push((byte >> bit) & 1)
+        for bit in (0..=7).rev() {
+            bits.push(1);                                   // Sync pulse
+            bits.push(((byte & (1 << bit)) != 0) as u8);    // Data bit
         }
     }
     bits
