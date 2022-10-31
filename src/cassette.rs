baaa58c724240878e63c5b9c8051229008e78152
Nicolas BAUW <nbauw@hotmail.com>
2022-10-31 23:30:28 +0100

    Mutex for tape image

--- src/cassette.rs
+++ src/cassette.rs
@@ -1,4 +1,4 @@
-use std::{thread, fs, fs::File, io::prelude::*, sync::Arc, sync::Mutex, time::Duration};
+use std::{thread, fs, fs::File, io::prelude::*, sync::Arc, sync::Mutex};
 use sdl2::keyboard::Keycode;
 use zilog_z80::crossbeam_channel::{Sender, Receiver};
 
@@ -26,6 +26,10 @@ pub fn launch(cassette_receiver: Receiver<(u8,u8)>, cassette_sender: Sender<(u8,
     let t_pos = Arc::clone(&pos);
     let t_pos1 = Arc::clone(&pos);
 
+    let bits = Arc::new(Mutex::new(load()));
+    let t_bits = Arc::clone(&bits);
+    let t_bits1 = Arc::clone(&bits);
+
     // 0xFF IO peripheral (Cassette) CPU -> Cassette
     thread::spawn(move || {
         loop {
@@ -40,10 +44,10 @@ pub fn launch(cassette_receiver: Receiver<(u8,u8)>, cassette_sender: Sender<(u8,
 
     // 0xFF IO peripheral (Cassette) Cassette -> CPU
     thread::spawn(move || {
-        let tape_bits = load();
         loop {
             if let Ok(device) = cassette_req.recv() {
                 let mut tape_pos = t_pos.lock().unwrap();
+                let tape_bits = t_bits.lock().unwrap();
                 // IN instruction for the 0xFF device ?
                 if device == 0xFF && *tape_pos < tape_bits.len() {
                     // We send the data through the io_in channel
@@ -61,7 +65,8 @@ pub fn launch(cassette_receiver: Receiver<(u8,u8)>, cassette_sender: Sender<(u8,
     thread::spawn(move || {
         loop {
             if let Ok(Keycode::F7) = cassette_cmd_rx.recv() {
-                //tape_bits = load();
+                let mut tape_bits = t_bits1.lock().unwrap();
+                *tape_bits = load();
                 let mut pos = t_pos1.lock().expect("Could not lock position counter");
                 *pos = 0;
             }
