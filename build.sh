238f5677248805a88f8a8a686e2b0d446e01c702
Nicolas BAUW <nbauw@hotmail.com>
2022-11-27 20:13:57 +0100

    Removed universal binary build script

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-# Apple universal binary build script
-cargo build --release --target=aarch64-apple-darwin
-cargo build --release --target=x86_64-apple-darwin
-lipo -create target/aarch64-apple-darwin/release/trust-80 target/x86_64-apple-darwin/release/trust-80 -output ./trust-80
-zip -9 teletype_mac_os.zip trust-80
