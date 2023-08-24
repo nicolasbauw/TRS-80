e3d5e364206299a95a3906b855cd1b01eb2c389d
Nicolas BAUW <nbauw@hotmail.com>
2023-08-24 18:24:34 +0200

    WIP keyboard

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use sdl2::{pixels::Color,event::Event,keyboard::Keycode, EventPump, render::Canvas, video::Window};
-use zilog_z80::{bus::Bus, cpu::CPU};
-use std::{error::Error, thread, time::Duration, collections::HashSet, fs};
 use crate::config::Config;
+use sdl2::{
+    event::Event,
+    keyboard::{self, Keycode},
+    pixels::Color,
+    render::Canvas,
+    video::Window,
+    EventPump,
+};
+use std::{collections::HashSet, error::Error, fs, thread, time::Duration};
+use zilog_z80::{bus::Bus, cpu::CPU};
 mod display;
-//mod keyboard;
+mod keyboard_device;
 //mod cassette;
 mod config;
 //mod console;
@@ -13,18 +20,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let config = config::load_config_file()?;
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
- 
-    let window = video_subsystem.window("TRuSt-80", config.display.width, config.display.height)
+
+    let window = video_subsystem
+        .window("TRuSt-80", config.display.width, config.display.height)
         .position_centered()
         .build()?;
- 
-    let mut canvas = window.into_canvas()
-        .accelerated()
-        .present_vsync()
-        .build()?;
+
+    let mut canvas = window.into_canvas().accelerated().present_vsync().build()?;
 
     let display_mode = video_subsystem.current_display_mode(0)?;
-    let refresh_rate =  display_mode.refresh_rate;
+    let refresh_rate = display_mode.refresh_rate;
 
     // Setting up CPU
     let bus = std::rc::Rc::new(std::cell::RefCell::new(Bus::new(config.memory.ram)));
@@ -32,47 +37,64 @@ fn main() -> Result<(), Box<dyn Error>> {
     c.debug.io = config.debug.iodevices.unwrap_or(false);
     c.debug.instr_in = config.debug.iodevices.unwrap_or(false);
     //c.debug.opcode = true;
-    if refresh_rate == 50 { 
-        c.set_slice_duration(20);       // Matching a 50 Hz refresh rate
+    if refresh_rate == 50 {
+        c.set_slice_duration(20); // Matching a 50 Hz refresh rate
     }
-    c.set_freq(1.77);               // Model 1 : 1.77 MHz
+    c.set_freq(1.77); // Model 1 : 1.77 MHz
     bus.borrow_mut().load_bin(&config.memory.rom, 0)?;
     let rom_space = fs::metadata(&config.memory.rom)?.len();
     bus.borrow_mut().set_romspace(0, (rom_space - 1) as u16);
     //let (vram_tx, vram_rx) = bounded(1);
 
-    loop {
+    let mut old_keys: HashSet<Keycode> = HashSet::new();
+
+    'running: loop {
         loop {
+            //println!("Start of CPU loop");
             // executes slice_max_cycles number of cycles
             if let Some(t) = c.execute_timed() {
                 //thread::sleep(Duration::from_millis(t.into()));
                 //println!("{t}");
                 break;
-            } 
+            }
         }
         let vram = bus.borrow().read_mem_slice(0x3C00, 0x4000);
         tape_device(bus.clone());
-        if !sdl_loop(&sdl_context, &mut canvas, vram, &config)? { break };
+        //if !sdl_loop(&sdl_context, &mut canvas, vram, &config)? { break };
+
+        //canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        display::display(&mut canvas, vram, &config).unwrap();
+        canvas.present();
+
+        let mut events = sdl_context.event_pump()?;
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::F12),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        let new_keys: HashSet<Keycode> = events
+            .keyboard_state()
+            .pressed_scancodes()
+            .filter_map(Keycode::from_scancode)
+            .collect();
+
+        let compare_keys = &new_keys - &old_keys;
+        let keys = match compare_keys.is_empty() {
+            true => new_keys.clone(),
+            false => old_keys,
+        };
+        old_keys = new_keys;
+
+        keyboard_device::keyboard(keys, bus.clone());
     }
     Ok(())
-}
-
-fn sdl_loop(ctx: &sdl2::Sdl, canvas: &mut Canvas<Window>, vram: Vec<u8>, config: &Config) -> Result<bool, Box<dyn Error>> {
-    //canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    display::display(canvas, vram, &config).unwrap();
-    canvas.present();
-    let mut events = ctx.event_pump()?;
-    for event in events.poll_iter() {
-        match event {
-            Event::Quit {..} |
-            Event::KeyDown { keycode: Some(Keycode::F12), .. } => {
-                return Ok(false)
-            },
-            _ => {}
-        }
-    }
-    Ok(true)
 }
 
 fn tape_device(bus: std::rc::Rc<core::cell::RefCell<Bus>>) {
