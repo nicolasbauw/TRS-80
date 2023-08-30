fd9a6c02d79446165c09ed92beaa85aff12f4168
Nicolas BAUW <nbauw@hotmail.com>
2023-08-30 20:03:04 +0200

    Simplifying the console thread

--- src/console.rs
+++ src/console.rs
@@ -21,30 +21,7 @@ pub fn launch(
                 let arg = parts.next();
                 let arg2 = parts.next();
 
-                match command {
-                    Some("tape") => match arg {
-                        Some(arg) => {
-                            cmd_channel.send((String::from("tape"), arg.to_string(), String::new()))?
-                        }
-                        None => {
-                            println!("No tape filename !")
-                        }
-                    },
-                    Some("d") => match arg {
-                        Some(address) => {
-                            cmd_channel.send((String::from("d"), address.to_string(), String::new()))?
-                        }
-                        None => {
-                            println!("No address !")
-                        }
-                    },
-                    Some("m") => {
-                        if arg2.is_none() { cmd_channel.send((String::from("m"), arg.unwrap().to_string(), String::new()))? } else {
-                            cmd_channel.send((String::from("m"), arg.unwrap().to_string(), arg2.unwrap().to_string()))?
-                        }
-                    },
-                    _ => cmd_channel.send((String::from(command.unwrap()), String::new(), String::new()))?,
-                }
+                cmd_channel.send((command.unwrap_or_default().to_string(), arg.unwrap_or_default().to_string(), arg2.unwrap_or_default().to_string()))?;
                 thread::sleep(Duration::from_millis(75));
             }
         },
