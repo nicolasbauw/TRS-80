4054bcf19d4275c2fb235bf89dfd4fce8171cfa3
Nicolas BAUW <nbauw@hotmail.com>
2023-08-30 17:56:22 +0200

    WIP command + 2 arguments

--- src/console.rs
+++ src/console.rs
@@ -1,10 +1,10 @@
 use std::{io::stdin, io::stdout, io::Write, sync::mpsc, thread, time::Duration};
 
 pub fn launch(
-    cmd_channel: mpsc::Sender<(String, String)>,
+    cmd_channel: mpsc::Sender<(String, String, String)>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     thread::Builder::new().name(String::from("Console")).spawn(
-        move || -> Result<(), mpsc::SendError<(String, String)>> {
+        move || -> Result<(), mpsc::SendError<(String, String, String)>> {
             loop {
                 print!("> ");
                 if stdout().flush().is_err() {
@@ -17,29 +17,33 @@ pub fn launch(
                 };
 
                 let mut parts = input.split_whitespace();
-                let Some(command) = parts.next() else {
-                    continue;
-                };
-                let args = parts;
+                let command = parts.next();
+                let arg = parts.next();
+                let arg2 = parts.next();
 
                 match command {
-                    "tape" => match args.peekable().peek() {
-                        Some(tape) => {
-                            cmd_channel.send((String::from(command), tape.to_string()))?
+                    Some("tape") => match arg {
+                        Some(arg) => {
+                            cmd_channel.send((String::from("tape"), arg.to_string(), String::new()))?
                         }
                         None => {
                             println!("No tape filename !")
                         }
                     },
-                    "d" => match args.peekable().peek() {
+                    Some("d") => match arg {
                         Some(address) => {
-                            cmd_channel.send((String::from(command), address.to_string()))?
+                            cmd_channel.send((String::from("d"), address.to_string(), String::new()))?
                         }
                         None => {
                             println!("No address !")
                         }
                     },
-                    _ => cmd_channel.send((String::from(command), String::new()))?,
+                    Some("m") => {
+                        if arg2.is_none() { cmd_channel.send((String::from("m"), arg.unwrap().to_string(), String::new()))? } else {
+                            cmd_channel.send((String::from("m"), arg.unwrap().to_string(), arg2.unwrap().to_string()))?
+                        }
+                    },
+                    _ => cmd_channel.send((String::from(command.unwrap()), String::new(), String::new()))?,
                 }
                 thread::sleep(Duration::from_millis(75));
             }
