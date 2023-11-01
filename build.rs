0ebabc04ef085886a9699e8c533b97c941286b66
Nicolas BAUW <nbauw@hotmail.com>
2023-11-01 16:37:19 +0100

    Does not overwrite config dir/file if exists

--- build.rs
+++ build.rs
@@ -5,15 +5,19 @@ fn main() {
     if let Some(user_dirs) = UserDirs::new() {
         let home_dir = user_dirs.home_dir();
         let mut dest = home_dir.to_path_buf();
-        let options = fs_extra::dir::CopyOptions::new();
+
+        // Do not overwrite config file if present
+        let options = fs_extra::dir::CopyOptions::new().skip_exist(true).overwrite(false);
 
         // Create .config directory in user's home directory
         dest.push(".config/");
         create(&dest, false).unwrap_or_default(); // Create error ? directory exists, so we don't care
 
-        // Create teletype's config directory in user's home directory
+        // Create trust-80's config directory in user's home directory
         dest.push("trust80/");
-        create(&dest, true).unwrap();
+        if let Err(_) = create(&dest, false) {
+            println!("~/.config/trust80 already exists")
+        };
 
         // Copy teletype/config.toml to home_dir/.config/teletype/config.toml
         let from_paths = vec!["config/config.toml"];
