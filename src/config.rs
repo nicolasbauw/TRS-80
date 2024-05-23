9b651ab6a3c7498305fb7751f69d4d5b4c31b093
Nicolas BAUW <nbauw@hotmail.com>
2024-05-23 21:26:12 +0200

    MachineError

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,8 @@ use directories::UserDirs;
 use serde_derive::Deserialize;
 use std::{fs, path::PathBuf};
 
+use crate::machine::MachineError;
+
 #[derive(Debug, Deserialize, Clone)]
 pub struct Config {
     pub display: ScreenConfig,
@@ -34,11 +36,11 @@ pub struct Debug {
     pub iodevices: Option<bool>,
 }
 
-pub fn load_config_file() -> Result<Config, std::io::Error> {
-    let user_dirs = UserDirs::new().ok_or(std::io::ErrorKind::Other);
+pub fn load_config_file() -> Result<Config, MachineError> {
+    let user_dirs = UserDirs::new().ok_or(MachineError::ConfigFile);
     let mut cfg = user_dirs?.home_dir().to_path_buf();
     cfg.push(".config/trust80/config.toml");
     let buf = fs::read_to_string(cfg)?;
-    let config: Config = toml::from_str(&buf).map_err(|_e| std::io::ErrorKind::Other)?;
+    let config: Config = toml::from_str(&buf).map_err(|_e| MachineError::ConfigFile)?;
     Ok(config)
 }
