42a09a7b4506939038b2a14f8ce00e766e674f61
Nicolas BAUW <nbauw@hotmail.com>
2026-07-05 20:44:18 +0200

    Comments

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -209,15 +209,15 @@ fn to_logical_scancode(physical: Scancode, azerty: bool, physical_shift: bool) -
         // --- LIGNE NUMÉRIQUE : Quand SHIFT n'est PAS pressé (L'utilisateur veut le SYMBOLE) ---
         // On renvoie la touche QWERTY équivalente et on FORCE le shift si nécessaire
         (Scancode::_1, false) => (Scancode::_6, true), // & (Shift + 7)
-        (Scancode::_2, false) => (Scancode::Apostrophe, false),
+        (Scancode::_2, false) => (Scancode::Apostrophe, false), // Nothing
         (Scancode::_3, false) => (Scancode::_2, true), // " (Shift + ')
         (Scancode::_4, false) => (Scancode::Apostrophe, false), // ' (Pas de shift)
         (Scancode::_5, false) => (Scancode::_8, true), // ( (Shift + 9)
-        (Scancode::_6, false) => (Scancode::Apostrophe, false),
-        (Scancode::_7, false) => (Scancode::Apostrophe, false),
+        (Scancode::_6, false) => (Scancode::Apostrophe, false), // Nothing
+        (Scancode::_7, false) => (Scancode::Apostrophe, false), // Nothing
         (Scancode::_8, false) => (Scancode::_1, true), // _ (Shift + -)
-        (Scancode::_9, false) => (Scancode::Apostrophe, false),
-        (Scancode::_0, false) => (Scancode::Apostrophe, false),
+        (Scancode::_9, false) => (Scancode::Apostrophe, false), // Nothing
+        (Scancode::_0, false) => (Scancode::Apostrophe, false), // Nothing
         (Scancode::Minus, false) => (Scancode::_0, true), // ) (Shift + 0)
 
         _ => (physical, physical_shift),
