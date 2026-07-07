e332bbec3a650720131a076d6378a4a8fec11b40
Nicolas BAUW <nbauw@hotmail.com>
2026-07-07 18:19:00 +0200

    zilog_z80 v0.19.0

--- src/main.rs
+++ src/main.rs
@@ -61,10 +61,10 @@ fn launch() -> Result<(), Box<dyn Error>> {
         }
 
         // Handle SDL keyboard events (keyboard MMIO peripheral)
-        trs80.keyboard.update(events, &mut trs80.cpu.bus, azerty);
+        trs80.keyboard.update(events, &mut trs80.bus, azerty);
 
         // Update display
-        trs80.display.update(&trs80.cpu.bus, &font)?;
+        trs80.display.update(&trs80.bus, &font)?;
 
         // Handle console commands
         trs80.console().unwrap_or_default();
